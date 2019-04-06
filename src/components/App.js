23f263ba909a60fdfed27758dffa56380fbcaf69
Bruno Wakiyama <takebo@gmail.com>
2019-04-05 22:20:38 -0300

    Moved Header and Footer components to App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Header from './templates/Header/Header';
+import Footer from './templates/Footer/Footer';
 import Home from './Home/Home';
 import Favorites from './Favorites/Favorites';
 
@@ -8,10 +10,12 @@ class App extends React.Component {
   render() {
     return (
       <Router>
+        <Header />
         <Switch>
           <Route path="/" exact component={Home} />
           <Favorites path="/favorites" exact component={Favorites} />
         </Switch>
+        <Footer />
       </Router>
     );
   }
