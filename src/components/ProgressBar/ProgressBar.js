8db07141da2fc68011d34b779b7c25423c3d3182
Bruno Wakiyama <takebo@gmail.com>
2019-04-07 14:50:40 -0300

    converted to function

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,16 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ProgressBar.scss';
 
-export default class ProgressBar extends React.Component {
-  render() {
-    return (
-      <div
-        className={this.props.complete ? 'progressbar complete' : 'progressbar'}
-      />
-    );
-  }
-}
+const ProgressBar = props => {
+  return (
+    <div className={props.complete ? 'progressbar complete' : 'progressbar'} />
+  );
+};
 
 ProgressBar.propTypes = {
   complete: PropTypes.bool,
 };
+
+export default ProgressBar;
